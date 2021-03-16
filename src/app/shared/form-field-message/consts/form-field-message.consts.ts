ee02a520295095f442c4fd2a19dbfb22f9921af6
Kelvin Marques <kelvin.marques@gmail.com>
2021-03-16 20:09:22 -0300

    Concluíndo a validação do formulário de cadastro

--- src/app/shared/form-field-message/consts/form-field-message.consts.ts
+++ src/app/shared/form-field-message/consts/form-field-message.consts.ts
@@ -1,6 +1,27 @@
 export const formFieldMessages = {
+    name: {
+        required: 'Informe seu Nome.',
+    },
+    lastname: {
+        required: 'Informe seu Sobrenome.',
+    },
+    email: {
+        required: 'Informe seu endereço de e-mail.',
+        email: 'Informe um endereço de e-mail válido.',
+    },
+    phone: {
+        required: 'Informe seu telefone fixo ou móvel.',
+        minlength: 'Informe um número de telefone válido com DDD.',
+    },
     cpf: {
-        required: 'Por favor, informe seu CPF.',
+        required: 'Informe seu CPF.',
         minlength: 'CPF incompleto.'
+    },
+    password: {
+        required: 'Cadastre sua senha de acesso ao aplicativo.'
+    },
+    passwordConfirm: {
+        required: 'Confirme sua senha de acesso ao aplicativo.',
+        pattern: 'A confirmação está diferente da senha informada.'
     }
 }
