b3b6531f590a71bcb1e345ce1dbc626d86f31901
Kelvin Marques <kelvin.marques@gmail.com>
2021-03-16 21:13:10 -0300

    Grid no formulário e melhoria do serviço viacep

--- src/app/sign-in/services/viacep.service.ts
+++ src/app/sign-in/services/viacep.service.ts
@@ -4,16 +4,14 @@ import { ViacepResponseInterface } from "../interfaces/viacep.interface";
 @Injectable()
 export class ViacepService {
 
+  private _endpoint = cep => `http://viacep.com.br/ws/${cep}/json`
+
   constructor(
   ) { }
 
   getAddress(cep: string): Promise<ViacepResponseInterface> {
     return fetch(`http://viacep.com.br/ws/${cep}/json`)
-      .then(response => {
-        if (response) {
-          return response.json();
-        }
-      })
+      .then(response => response.json())
       .catch(error => console.error(`Erro ao consultar o viacep: ${error.status}: ${error.error}. headers: ${error.headers}`));
   }
 }
